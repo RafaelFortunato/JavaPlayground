55d97ca7e0da86229c875ffefd6ef9f4858b1b39
Rafael Paz <rafael.paz@keyrus.com.br>
2019-01-11 12:27:16 -0200

    Bipredicate tests

--- src/PredicateTest.java
+++ src/PredicateTest.java
@@ -1,12 +1,22 @@
+import java.util.*;
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
+import static java.util.Collections.*;
 
 public class PredicateTest {
 
     public static class Character {
-        int hp = 50;
+        static int hp = 50;
 
         public boolean isAlive(Predicate<Integer> formula) {
             return formula.test(hp);
+        }
+    }
+
+    public static class CustomComparator implements Comparator<Character> {
+        @Override
+        public int compare(Character o1, Character o2) {
+            return 0;
         }
     }
 
@@ -20,12 +30,24 @@ public class PredicateTest {
             return i == 5;
         }));
 
+        System.out.println("CallTest2: " + callTest2(8, 5, (a, b) -> a > b));
+
         Character c = new Character();
         System.out.println("IsAlive: " + c.isAlive(i -> i > 0));
+
+        Character nullC = null;
+        System.out.println(nullC.hp);
+
+        ArrayList<Character> chars = new ArrayList<>();
+        sort(chars, new CustomComparator());
     }
 
     private static boolean callTest(Predicate<Integer> p) {
         return p.test(5);
+    }
+
+    private static boolean callTest2(int a, int b, BiPredicate<Integer, Integer> p) {
+        return p.test(a, b);
     }
 
 }
