10c6b198e4c6a3777fc802e875683baf4cc02c82
Rafael Paz <rafael.paz@keyrus.com.br>
2019-01-14 15:01:26 -0200

    Hierarchy without default constructor

--- src/Chapter5/ExtendParentWODefaultConstructor.java
+++ src/Chapter5/ExtendParentWODefaultConstructor.java
@@ -1,4 +1,24 @@
 package Chapter5;
 
 public class ExtendParentWODefaultConstructor {
+    public ExtendParentWODefaultConstructor(int arg) {
+
+    }
+
+//    public class Child extends ExtendParentWODefaultConstructor {
+//         // DOES NOT COMPILE
+//         // There is no default constructor available in ExtendParentWODefaultConstructor
+//    }
+
+//    public class Child2 extends ExtendParentWODefaultConstructor {
+//        public Child2() {
+//            // DOES NOT COMPILE
+//        }
+//    }
+
+    public class Child3 extends ExtendParentWODefaultConstructor {
+        public Child3() {
+            super(10);
+        }
+    }
 }
